75930890340aba20be21b516032ac358ac8c820e
Edgardo Campos (Xotl) <edgardo333@gmail.com>
2015-07-25 21:44:42 -0500

    updating WIP

---
+++ actions/getData.js
@@ -0,0 +1,30 @@
+exports.getData = {
+  name: 'getData',
+  description: 'I am an API method which will generate a random number',
+  inputs: {
+  },
+  outputExample: {
+
+    "serverInformation": {
+      "serverName": "actionhero API",
+      "apiVersion": "0.0.1",
+      "requestDuration": 2,
+      "currentTime": 1437872679509
+    },
+    "requesterInformation": {
+      "id": "c4311e095dbc0b8fb4dd23d6d7a9ceb378270e97-ce117798-fbb6-4af6-be46-1c296665709c",
+      "fingerprint": "c4311e095dbc0b8fb4dd23d6d7a9ceb378270e97",
+      "remoteIP": "200.52.194.71",
+      "receivedParams": {
+        "apiVersion": "1",
+        "action": "getData"
+      }
+    }
+  },
+
+  run: function(api, data, next){
+    data.response.status = 'OK';
+    next(null);
+  }
+
+};
--- actions/postUserData.js
+++ actions/postUserData.js
@@ -1,3 +1,7 @@
+var request = require('request');
+var FIWARE_BASE_URL = process.env.FIWARE_BASE_URL || "http://orion.lab.fi-ware.org:1026/";
+var FIWARE_AUTH_TOKEN = process.env.FIWARE_AUTH_TOKEN;
+
 exports.postUserData = {
   name: 'postUserData',
   description: 'I am an API method which will generate a random number',
@@ -9,6 +13,9 @@ exports.postUserData = {
       required: true
     },
     long: {
+      required: true
+    },
+    alt: {
       required: true
     }
   },
@@ -31,9 +38,60 @@ exports.postUserData = {
     }
   },
 
-  run: function(api, data, next){
-    data.response.status = 'OK';
-    next(null);
+  run: function(api, data, next) {
+
+    var options = {
+      url: FIWARE_BASE_URL + "ngsi10/updateContext/",
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'X-Auth-Token': FIWARE_AUTH_TOKEN
+      },
+      json: true,
+      body: {
+        "contextElements": [
+            {
+                "type": "prueba",
+                "isPattern": "false",
+                "id": "cp:guadalajara:2015",
+                "attributes": [
+                  {
+                      "name": "lat",
+                      "type": "float",
+                      "value": data.params.lat
+                  },
+                  {
+                      "name": "long",
+                      "type": "float",
+                      "value": data.params.long
+                  },
+                  {
+                      "name": "price",
+                      "type": "float",
+                      "value": data.params.price
+                  }
+                ]
+            }
+        ],
+        "updateAction": "APPEND"
+      }
+    };
+
+
+    console.log(options.body.contextElements[0].attributes);
+    request(options, function (error, response, body) {
+      console.log('Request has finished!', error);
+
+      if (!error && response.statusCode == 200) {
+        data.response.fiware_response = body;
+        data.response.status = 'OK';
+
+        console.log(body); // Show the HTML for the Google homepage.
+      }
+
+      next(error);
+    });
   }
 
 };
