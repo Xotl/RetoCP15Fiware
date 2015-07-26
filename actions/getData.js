361f7b40232591554478804f110a37f92519755a
Edgardo Campos (Xotl) <edgardo333@gmail.com>
2015-07-26 00:56:05 -0500

    First RC version

--- actions/getData.js
+++ actions/getData.js
@@ -1,3 +1,29 @@
+var request = require('request');
+var FIWARE_BASE_URL = process.env.FIWARE_BASE_URL || "http://orion.lab.fi-ware.org:1026/";
+var FIWARE_AUTH_TOKEN = process.env.FIWARE_AUTH_TOKEN || '851vrEfHL3fb9wAH5FprWqYuQRrxej';
+var queueLiters = [], queueUserData = [];
+var headers = {
+  'Accept': 'application/json',
+  'Content-type': 'application/json',
+  'X-Auth-Token': FIWARE_AUTH_TOKEN
+};
+
+var orion = {
+  getOrionData: function(cb) {
+    var options = {
+      url: FIWARE_BASE_URL + 'ngsi10/contextEntities/cp:guadalajara:2015',
+      method: 'GET',
+      headers: headers
+    };
+
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        cb(error, body);
+      }
+    });
+  }
+};
+
 exports.getData = {
   name: 'getData',
   description: 'I am an API method which will generate a random number',
@@ -23,10 +49,16 @@ exports.getData = {
   },
 
   run: function(api, data, next) {
-    api.config['my-plugin'].getOrionData('good', function(err, data) {
-      data.response.status = 'OK';
-      data.response.fiware_response = data;
-      next(err);
+
+    orion.getOrionData(function(err, res) {
+      res = JSON.parse(res);
+      data.response.fiware_response = res
+      data.response.result = {};
+      res.contextElement.attributes.forEach(function(entry) {
+        data.response.result[entry.name] = entry.value;
+      });
+
+      next(err)
     });
   }
 
--- actions/postUserData.js
+++ actions/postUserData.js
@@ -1,15 +1,118 @@
+var request = require('request');
+var FIWARE_BASE_URL = process.env.FIWARE_BASE_URL || "http://orion.lab.fi-ware.org:1026/";
+var FIWARE_AUTH_TOKEN = process.env.FIWARE_AUTH_TOKEN || '851vrEfHL3fb9wAH5FprWqYuQRrxej';
+var queueLiters = [], queueUserData = [];
+var headers = {
+  'Accept': 'application/json',
+  'Content-type': 'application/json',
+  'X-Auth-Token': FIWARE_AUTH_TOKEN
+};
+
+var orion = {
+  fromPostLiters: function(lts) {
+    queueLiters.push(lts);
+    this.updateOrion();
+  },
+  fromPostUser: function(data) {
+    queueUserData.push(data);
+    this.updateOrion();
+  },
+  getOrionData: function(cb) {
+    var options = {
+      url: FIWARE_BASE_URL + 'ngsi10/contextEntities/cp:guadalajara:2015',
+      method: 'GET',
+      headers: headers
+    };
+
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        cb(error, body);
+      }
+    });
+  },
+  updateOrion: function(cb) {
+    if (queueLiters.length === 0 || queueUserData.length === 0) {
+      return;
+    }
+
+
+    this.getOrionData(function(err, res) {
+      res = JSON.parse(res).contextElement;
+      var obj = {};
+      res.attributes.forEach(function(entry) {
+        obj[entry.name] = entry.value;
+      });
+
+      var temp = queueUserData.shift();
+      console.log("asdsaddpppppppppppppppppp",temp);
+      var options = {
+        url: FIWARE_BASE_URL + "ngsi10/updateContext/",
+        method: 'POST',
+        headers: headers,
+        json: true,
+        body: {
+          "contextElements": [
+              {
+                  "type": "",
+                  "isPattern": "false",
+                  "id": "cp:guadalajara:2015",
+                  "attributes": [
+                    {
+                        "name": "lat",
+                        "type": "string",
+                        "value": obj.lat + ', ' + temp.lat
+                    },
+                    {
+                        "name": "long",
+                        "type": "string",
+                        "value": obj.long + ', ' + temp.long
+                    },
+                    {
+                        "name": "price",
+                        "type": "string",
+                        "value": obj.price + ', ' + temp.price
+                    },
+                    {
+                        "name": "liters",
+                        "type": "string",
+                        "value": obj.liters + ', ' + queueLiters.shift()
+                    }
+                  ]
+              }
+          ],
+          "updateAction": "UPDATE"
+        }
+      };
+
+      request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          if (cb) {
+            cb(error, body);
+          }
+        }
+      });
+
+    });
+  }
+
+};
+
+
 exports.postUserData = {
   name: 'postUserData',
   description: 'Send the price and geocode from the user.',
   inputs: {
     price: {
-      required: true
+      required: false
     },
     lat: {
-      required: true
+      required: false
     },
     long: {
-      required: true
+      required: false
+    },
+    liters: {
+      required: false
     }
   },
   outputExample: {
@@ -33,15 +136,23 @@ exports.postUserData = {
 
   run: function(api, data, next) {
 
-    var obj = {
-      lat: data.params.lat,
-      long: data.params.long,
-      price: data.params.price
+    if (data.params.liters) {
+      orion.fromPostLiters(data.params.liters);
+      data.response.status = "OK";
+      next(null);
     }
+    else {
 
-    api.config['my-plugin'].fromPostUser(obj);
-    data.response.status = "OK";
-    next(null);
+      var obj = {
+        lat: data.params.lat,
+        long: data.params.long,
+        price: data.params.price
+      }
+
+      orion.fromPostUser(obj);
+      data.response.status = "OK";
+      next(null);
+    }
   }
 
 };
